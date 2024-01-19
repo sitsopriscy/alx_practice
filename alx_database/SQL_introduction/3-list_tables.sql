737092cb5fb111e91021c9297f6dc69bd35235f4
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2024-01-19 09:31:43 -0800

    SQL - Introduction

---
+++ alx_database/SQL_introduction/3-list_tables.sql
@@ -0,0 +1,9 @@
+-- Write a script that lists all the tables of a database in your MySQL server.
+-- The database name will be passed as argument of mysql command (in the following example: mysql is the name of the database)
+ 
+USE hbtn_0c_0;
+
+SHOW TABLES;
+or
+SHOW FULL TABLES;
+
